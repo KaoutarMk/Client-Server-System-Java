1dc1e7fd891eb4e0221c51f787a48c459184c5d6
Kaoutar Machouat <149963132+KaoutarMk@users.noreply.github.com>
2025-02-05 16:34:29 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,1 +1,32 @@
+package main;
 
+public class Driver {
+
+	/** 
+	 * main class
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		
+	   	
+	   	 /*******************************************************************************************************************************************
+	   	  * TODO : implement all the operations of main class   																					*
+	   	  ******************************************************************************************************************************************/
+	    
+		// Initialize all the threads
+		
+	   	Network network = new Network("network");		/* Activate the network */
+	    network.start();
+	    
+	    Server server = new Server();                        /* Start the server */ 
+        server.start();
+        
+        Client clientSend = new Client("sending");              /* Start the sending client */
+        clientSend.start();
+        
+        Client clientReceive = new Client("receiving");            /* Start the receiving client */
+        clientReceive.start();
+		
+	    
+	}
+}
